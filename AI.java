ea6cc4dcebf57aae81a082e94c8c1057a8470c24
lewisforbes <60221117+lewisforbes@users.noreply.github.com>
2020-04-06 09:45:55 +0100

    Add files via upload

---
+++ AI.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AI {
+
+    public static ArrayList<String> moves;
+
+    private static HashMap<Integer, Integer> disksPos = new HashMap();
+    private static int[] polesTopLayer = new int[3];
+    private static int largeDisk;
+
+    public static void setMoves(int numOfDisks) {
+        ArrayList<String> output = new ArrayList<>();
+        initialisePositions(numOfDisks);
+
+        int reqMoves = (int) (Math.pow(2, numOfDisks)-1);
+        int finalBinLength = Integer.toBinaryString(reqMoves).length();
+        String currentBinNum = mkRightLength(finalBinLength, "0");
+        String nextBinNum = mkRightLength(finalBinLength, binaryPlusOne(currentBinNum));
+
+        output.add(moveToMake(currentBinNum, nextBinNum));
+
+        while (output.size() < reqMoves) {
+            currentBinNum = nextBinNum;
+            nextBinNum = mkRightLength(finalBinLength, binaryPlusOne(currentBinNum));
+            output.add(moveToMake(currentBinNum, nextBinNum));
+        }
+
+        moves = output;
+    }
+
+    private static String moveToMake(String current, String next) {
+        int changedIndex = -1;
+        int length = current.length();
+
+        for (int i = 0; i < length; i++) {
+            if (current.charAt(i) != next.charAt(i)) {
+                changedIndex = i;
+                break;
+            }
+        }
+        if (changedIndex == -1) {
+            throw new IllegalArgumentException("The index remained unchanged.");
+        }
+
+        int diskToMove = length - changedIndex;
+        int currentPos;
+        int newPos;
+        if (diskToMove == 1) {
+            currentPos = disksPos.get(1);
+            newPos = currentPos+1;
+            if (newPos == 4) {
+                newPos = 1;
+            }
+            updatePositions(diskToMove, currentPos, newPos);
+            return currentPos + " " + newPos;
+        }
+
+        currentPos = disksPos.get(diskToMove);
+        newPos = dest(diskToMove, currentPos);
+        updatePositions(diskToMove, currentPos, newPos);
+        return currentPos + " " + newPos;
+    }
+
+    private static int dest(int disk, int currentPos) {
+        int potentialMove = currentPos+1;
+        if (potentialMove==4) {
+            potentialMove = 1;
+        }
+        if (disk < polesTopLayer[potentialMove-1]) {
+            return potentialMove;
+        }
+
+        potentialMove++;
+        if (potentialMove==4) {
+            potentialMove = 1;
+        }
+        if (disk < polesTopLayer[potentialMove-1]) {
+            return potentialMove;
+        }
+
+        throw new IllegalArgumentException("The disk could not be moved.");
+    }
+
+    private static void updatePositions(int diskToMove, int currentPos, int newPos) {
+        disksPos.put(diskToMove, newPos);
+        polesTopLayer[newPos-1] = diskToMove;
+
+        int smallestDisk = largeDisk;
+        for (Integer disk : disksPos.keySet()) {
+            if (disksPos.get(disk) == currentPos) {
+                if (disk < smallestDisk) {
+                    smallestDisk = disk;
+                }
+            }
+        }
+
+        polesTopLayer[currentPos-1] = smallestDisk;
+    }
+
+    private static void initialisePositions(int numOfDisks) {
+        largeDisk = numOfDisks+1;
+        for (int i=1; i<=numOfDisks; i++) {
+            disksPos.put(i, 1);
+        }
+
+        polesTopLayer[0] = 1;
+        polesTopLayer[1] = largeDisk;
+        polesTopLayer[2] = largeDisk;
+    }
+
+    private static String binaryPlusOne(String binaryNum) {
+        int output = Integer.parseInt(binaryNum,2);
+        output ++;
+        return Integer.toBinaryString(output);
+    }
+
+    private static String mkRightLength(int length, String binNum) {
+        if (binNum.length() > length) {
+            throw new IllegalArgumentException("Number is too big.");
+        }
+
+        if (binNum.length() == length) {
+            return binNum;
+        }
+
+        return "0".repeat(length-binNum.length()) + binNum;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public enum player {ME, AI}
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Towers of Hanoi!\n");
+        System.out.println("Welcome to Towers of Hanoi!");
         initialiseGame();
     }
 
@@ -11,10 +14,32 @@ public class Main {
         int numOfDisks = 0;
         Scanner input = new Scanner(System.in);
         boolean proceed = false;
+        String inputtedLine;
 
+        player chosenPlayer = player.ME;
+        while ((!proceed) && (numOfTowers==3)) {
+            System.out.println("\nType '" + player.AI + "' to see computer solve, or '" + player.ME + "' to solve yourself: ");
+            inputtedLine = input.nextLine();
+
+            if (player.AI.name().equalsIgnoreCase(inputtedLine.strip())) {
+                chosenPlayer = player.AI;
+                proceed = true;
+            }
+
+            if (player.ME.name().equalsIgnoreCase(inputtedLine.strip())) {
+                chosenPlayer = player.ME;
+                proceed = true;
+            }
+
+            if (!proceed) {
+                System.err.println("Enter either '" + player.AI + "' or '" + player.ME + ".\n");
+            }
+        } // human or AI
+
+        proceed = false;
         while (!proceed) {
-            System.out.println("Enter the number of disks to play with: ");
-            String inputtedLine = input.nextLine();
+            System.out.println("\nEnter the number of disks to play with: ");
+            inputtedLine = input.nextLine();
             try {
                 int i = Integer.parseInt(inputtedLine.replaceAll(" ", ""));
                 proceed = true;
@@ -29,8 +54,12 @@ public class Main {
                     proceed = false;
                 }
             }
+        } // disks to play with
+
+        if (numOfDisks == 0) {
+            throw new IllegalArgumentException("Number of disks has not been set.");
         }
 
-        NewGame game = new NewGame(numOfTowers, numOfDisks);
+        NewGame game = new NewGame(numOfTowers, numOfDisks, chosenPlayer);
     }
 }
